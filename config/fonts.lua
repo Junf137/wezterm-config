8063febd1aaf1266bee8670d55f689fd016f585d
Junfeng Lei <junf137@outlook.com>
2025-11-02 15:38:18 -0500

    feat: config font

--- config/fonts.lua
+++ config/fonts.lua
@@ -2,13 +2,13 @@ local wezterm = require('wezterm')
 local platform = require('utils.platform')
 
 -- local font = 'Maple Mono SC NF'
-local font_family = 'JetBrainsMono Nerd Font'
-local font_size = platform.is_mac and 12 or 9
+local font_family = 'FiraCode Nerd Font Mono'
+local font_size = platform.is_mac and 12 or platform.is_win and 12 or platform.is_linux and 12 or 12
 
 return {
    font = wezterm.font({
       family = font_family,
-      weight = 'Medium',
+      weight = 'DemiLight',
    }),
    font_size = font_size,
 
