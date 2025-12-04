b2cfd870fb9db09ad574489ce944d85cfb211f6e
Junfeng Lei <junf137@outlook.com>
2025-12-04 17:26:47 +0800

    feat: toggle_fullscreen for mac

--- events/gui-startup.lua
+++ events/gui-startup.lua
@@ -1,12 +1,21 @@
 local wezterm = require('wezterm')
 local mux = wezterm.mux
+local platform = require('utils.platform')
 
 local M = {}
 
 M.setup = function()
    wezterm.on('gui-startup', function(cmd)
       local _, _, window = mux.spawn_window(cmd or {})
-      window:gui_window():maximize()
+      local gui_window = window:gui_window()
+      
+      if platform.is_mac then
+         -- On macOS, toggle_fullscreen works more reliably than maximize
+         gui_window:toggle_fullscreen()
+      else
+         -- On Linux and Windows, maximize works fine
+         gui_window:maximize()
+      end
    end)
 end
 
